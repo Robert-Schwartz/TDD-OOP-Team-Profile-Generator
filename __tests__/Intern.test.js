9d40481641f33b0a71c3b9f3ee2975ca814b7b6d
Robert Schwartz <bschwartz23@gmail.com>
2021-05-15 13:20:44 -0700

    create failing tests for Intern sublcass:  In addition to Employee's properties and methods, Intern will also have:
    school
    
    getSchool()
    
    getRole() // Overridden to return 'Intern'

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,21 @@
 //--------------Require Intern.js file to test
 const Intern = require('../lib/Intern');
+
+
+//--------------Test for Intern school parameter
+test('test for intern subclass to add school parameter', () => {
+    const intern = new Intern('Bob', 7411, 'bschwartz23@gmail.com', 'UA');
+    expect(intern.school).toBe('UA');
+});
+
+//--------------Test for Intern to return GitHub username value
+test('test for Intern to return school value', () => {
+    const intern = new Intern('Bob', 7411, 'bschwartz23@gmail.com','UA');
+    expect(intern.getSchool()).toBe('UA');
+});
+
+//--------------Test for manager role to be overridden to "Intern"
+test('test for intern role to be overridden to "Intern"', () => {
+    const intern = new Intern('Bob', 7411, 'bschwartz23@gmail.com', 'UA',);
+    expect(intern.getRole()).toBe('Intern');
+});
