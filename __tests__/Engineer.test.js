0a2e8cebc6773a608622f41de1621c7a6ecde4f6
Robert Schwartz <bschwartz23@gmail.com>
2021-05-15 13:04:16 -0700

    create Engineer Subclass to pass all tests:    ✓ test for engineer subclass to add GitHub username parameter (2 ms)   ✓ test for Engineer to return GitHub username value (1 ms)   ✓ test for engineer role to be overridden to Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,13 +8,13 @@ test('test for engineer subclass to add GitHub username parameter', () => {
 });
 
 //--------------Test for Engineer to return GitHub username value
-test('est for Engineer to return GitHub username value', () => {
+test('test for Engineer to return GitHub username value', () => {
     const engineer = new Engineer('Bob', 7411, 'bschwartz23@gmail.com', 'Robert-Schwartz');
-    expect(engineer.getGithub()).toBe('Engineer');
+    expect(engineer.getGithub()).toBe('Robert-Schwartz');
 });
 
 //--------------Test for manager role to be overridden to "Engineer"
 test('test for engineer role to be overridden to "Engineer"', () => {
-    const manager = new Manager('Bob', 7411, 'bschwartz23@gmail.com', 'Robert-Schwartz',);
-    expect(manager.getGithub()).toBe('Engineer');
+    const engineer = new Engineer('Bob', 7411, 'bschwartz23@gmail.com', 'Robert-Schwartz',);
+    expect(engineer.getRole()).toBe('Engineer');
 });
